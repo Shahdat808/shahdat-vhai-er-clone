6805dc5622dd43e0a4bb631fb6a868e71b0ef16e
Shahdat808 <110221185+Shahdat808@users.noreply.github.com>
2022-07-29 12:49:30 +0600

    Add files via upload

---
+++ freeclone.py
@@ -0,0 +1,12 @@
+print("SHAHDAT VHAI")
+print("Facebook New id for cloning")
+print("Contact:rssrabonihossain@gmail.com")
+
+print("Loading............!")
+
+
+print("Loading.............!")
+print("azankozam@exdonuts.com")
+print("ILOVEALLAH1")
+
+print("Gmail me for more Free ids")
